9efe2befb61b5ce9cef7105a378392a58099b1e0
Jiří Otáhal <xotahal@gmail.com>
2019-03-18 20:03:38 +1300

    tests: remove default tests

--- spec/semantic_release_action_spec.rb
+++
@@ -1,9 +0,0 @@
-describe Fastlane::Actions::SemanticReleaseAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The semantic_release plugin is working!")
-
-      Fastlane::Actions::SemanticReleaseAction.run(nil)
-    end
-  end
-end
