3b1f54d4038a8c10b8af4be762dce2ff899f4540
Luke van Tricht <luke.vantricht@blake.com.au>
2019-12-09 13:16:49 +1000

    Added an Abbrev option to `analyze_commits`, which allows for appending `--abbrev=N` to the git describe command, to change how analyze_commits chooses the tag to compare against.

--- lib/fastlane/plugin/semantic_release/actions/analyze_commits.rb
+++ lib/fastlane/plugin/semantic_release/actions/analyze_commits.rb
@@ -20,7 +20,9 @@ module Fastlane
     class AnalyzeCommitsAction < Action
       def self.get_last_tag(params)
         # Try to find the tag
-        command = "git describe --tags --match=#{params[:match]}"
+        command = "git describe --tags --match='#{params[:match]}'"
+        command +=  " --abbrev=#{params[:abbrev]}" if params[:abbrev] != -1
+        UI.message(command)
         Actions.sh(command, log: params[:debug])
       rescue
         UI.message("Tag was not found for match pattern - #{params[:match]}")
@@ -45,7 +47,11 @@ module Fastlane
         # command to get first commit
         git_command = 'git rev-list --max-parents=0 HEAD'
 
-        tag = get_last_tag(match: params[:match], debug: params[:debug])
+        tag = get_last_tag(
+          match: params[:match],
+          abbrev: params[:abbrev],
+          debug: params[:debug]
+        )
 
         # if tag doesn't exist it get's first commit or fallback tag (v*.*.*)
         if tag.empty?
@@ -335,6 +341,12 @@ module Fastlane
             default_value: false,
             type: Boolean,
             optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :abbrev,
+            description: "Abbrev parameter of git describe. See man page of git describe for more info",
+            default_value: -1,
+            type: Integer
           )
         ]
       end
