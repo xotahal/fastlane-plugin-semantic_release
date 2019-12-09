003e0321c9a50cb2f262bef8766401f56b3ae519
Luke van Tricht <luke.vantricht@blake.com.au>
2019-12-09 13:33:32 +1000

    Removed accidental debug loggins

--- lib/fastlane/plugin/semantic_release/actions/analyze_commits.rb
+++ lib/fastlane/plugin/semantic_release/actions/analyze_commits.rb
@@ -22,7 +22,6 @@ module Fastlane
         # Try to find the tag
         command = "git describe --tags --match='#{params[:match]}'"
         command +=  " --abbrev=#{params[:abbrev]}" if params[:abbrev] != -1
-        UI.message(command)
         Actions.sh(command, log: params[:debug])
       rescue
         UI.message("Tag was not found for match pattern - #{params[:match]}")
