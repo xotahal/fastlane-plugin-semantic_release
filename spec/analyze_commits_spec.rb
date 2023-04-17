b86d39f2bd3cfc76bb3a6aea95ac7dbc0520cfc0
Stanislav Gribushin <stanislav@jigx.com>
2023-04-17 07:19:29 +0200

    chore: fix lint

--- spec/analyze_commits_spec.rb
+++ spec/analyze_commits_spec.rb
@@ -12,7 +12,7 @@ describe Fastlane::Actions::AnalyzeCommitsAction do
       allow(Fastlane::Actions::AnalyzeCommitsAction).to receive(:get_commits_from_hash).and_return(commits)
     end
 
-    def test_analyze_commits_same_commit_as_tag()
+    def test_analyze_commits_same_commit_as_tag
       # for simplicity, these two actions are grouped together because they need to be run for every test,
       # but require different commits to be passed each time. So we can't use the "before :each" for this
       # this is the same as test_analyze_commits, but the last commit is the same as the last tag
@@ -174,13 +174,13 @@ describe Fastlane::Actions::AnalyzeCommitsAction do
       expect(execute_lane_test(match: 'v*')).to eq(false)
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::RELEASE_NEXT_VERSION]).to eq("1.0.8")
     end
-  
+
     it "should return false when we are on the same commit as the last tag" do
       commits = [
         "Merge ...|",
         "Custom ...|"
       ]
-      test_analyze_commits_same_commit_as_tag()
+      test_analyze_commits_same_commit_as_tag
 
       expect(execute_lane_test(match: 'v*')).to eq(false)
       expect(Fastlane::Actions.lane_context[Fastlane::Actions::SharedValues::RELEASE_NEXT_VERSION]).to eq("1.0.8")
